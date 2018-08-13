a6303cc5b6f771027d76d27a0a9fc491b4f4f7ae
slr376 <slr376@drexel.edu>
2018-08-13 17:27:26 -0400

    Added sentiment plotting

--- Master.R
+++ Master.R
@@ -53,7 +53,7 @@ for (id in AmzID) {
       html_text() %>%
       str_extract("\\d") %>%
       as.numeric()
-    AMZtemp <- data.frame(prod, body, stars)
+    Amztemp <- data.frame(prod, body, stars)
     AmzRevDF <- rbind(AmzRevDF, Amztemp)
   }
 }
@@ -104,4 +104,65 @@ for (game in games) {
 MCRevDF$stars <- as.numeric(MCRevDF$stars)
 
 masterDF <- rbind(MCRevDF, AmzRevDF)
+masterDF[masterDF$prod == 'Star Wars Battlefront II - Xbox One',] <- 'star-wars-battlefront-ii'
+masterDF[masterDF$prod == 'Star Wars: Battlefront - Standard Edition - Xbox One',] <- 'star-wars-battlefront'
+masterDF[masterDF$prod == 'Call of Duty: WWII - PlayStation 4 Standard Edition',] <- 'call-of-duty-wwii'
+masterDF[masterDF$prod == 'Electronic Arts Battlefield 1 - Xbox One',] <- 'battlefield-1'
 
+bfDF <-masterDF[masterDF$prod == 'battlefield-1', ]
+swbfiiDF <-masterDF[masterDF$prod == 'star-wars-battlefront-ii', ]
+swbfDF <-masterDF[masterDF$prod == 'star-wars-battlefront', ]
+codDF <-masterDF[masterDF$prod == 'call-of-duty-wwii', ]
+
+masterDF$body <-as.character(masterDF$body)
+
+#-----Sentiment Analysis
+tidyGame <- masterDF %>%
+  group_by(prod) %>%
+  mutate(index = row_number()) %>%
+  ungroup() %>%
+  unnest_tokens(word, body)
+
+data("stop_words")
+tidyGame <- tidyGame %>%
+  anti_join(stop_words)
+
+extraWords <- bind_rows(data_frame(word = c("game", 'play', 'star', 'wars', 'bf1', 'bf2', 'players', "battlefront", "playing", "ea", 'kill', 'death','assault', 'damage', 'like', 'gift', 'fun', 'nice', 'amazing', 'awesome', '4', 'xbox', 'loves', 'battle', 'son', '3', '10', 'reviews', 'pretty', 'grandson', '1'), 
+                                   lexicon = c("custom")), 
+                        stop_words)
+tidyGame <- tidyGame %>%
+  anti_join(extraWords)
+
+gameSent <- tidyGame %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(prod, index = index %/% 4, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+ggplot(gameSent, aes(index, sentiment, fill = prod)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~prod, ncol = 2, scales = "free_x") +
+  labs(title='Title')
+
+wordCount <- tidyGame %>%
+  group_by(prod) %>%
+  summarize(word = n())
+
+bingChart <- tidyGame %>%
+  right_join(get_sentiments('bing')) %>%
+  group_by(prod) %>%
+  count(sentiment)
+
+bingChart <- bingChart[-c(9, 10), ]
+
+ggplot(bingChart, aes(fill=sentiment, ymax=cumsum(bingChart$n), ymin=c(0, head(cumsum(bingChart$n), n=-1)), xmax=4, xmin=3)) +
+  geom_rect(colour="grey30") +
+  facet_wrap(~prod, ncol = 2) +
+  coord_polar(theta="y") +
+  xlim(c(0, 4)) +
+  theme_bw() +
+  theme(panel.grid=element_blank()) +
+  theme(axis.text=element_blank()) +
+  theme(axis.ticks=element_blank()) +
+  labs(title="Positive vs. Negative")
+#-----Topic Modeling
